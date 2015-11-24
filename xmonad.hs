9921e1a9e56b3489cfaa006824e18d27e7a9dd02
Anthony Parks <acousticscommander@gmail.com>
2015-11-24 15:00:38 -0500

    Update xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -1,1 +1,11 @@
+import XMonad
 
+main = do
+   xmonad $ defaultConfig
+     {
+	borderWidth = myBorderWidth,
+	focusedBorderColor = myFocusedBorderColor
+     }
+
+myBorderWidth = 1
+myFocusedBorderColor = "#000000"
