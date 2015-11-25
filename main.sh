d6846502ca1d841443425c3445b1300b1c11c8a1
Anthony Parks <acousticscommander@gmail.com>
2015-11-25 15:55:39 -0500

    Update main.sh
    now running commands that don't need to be run by superuser as user
    http://stackoverflow.com/questions/10220019/how-to-write-a-shell-script-that-runs-some-commands-as-superuser-and-some-comman

--- main.sh
+++ main.sh
@@ -1,11 +1,12 @@
 # must run as superuser, but in home directory
 aptitude install build-essential python idle3 emacs xmonad dzen2 git thunar gnome-icon-theme-full lxappearance
-cp .emacs ~/
-mkdir ~/.xmonad
+sudo -u ${USERNAME} cp .emacs ~/
+sudo -u ${USERNAME} mkdir ~/.xmonad
 cp xmonad.hs ~/
 
 # install source code pro font
-chmod u+x installSourceCodePro.hs
-sh installSourceCodePro.hs
+sudo -u ${USERNAME} chmod u+x installSourceCodePro.hs
+sudo -u ${USERNAME} sh installSourceCodePro.hs
 
 # todo: copy xsession
+sudo -u ${USERNAME} cp .xsession ~/
