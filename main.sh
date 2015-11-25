26d71419fef4ed396ef574939bc5f8ed29a4212e
Anthony Parks <acousticscommander@gmail.com>
2015-11-25 15:19:24 -0500

    Update main.sh
    move repo files to appropriate places

--- main.sh
+++ main.sh
@@ -1,2 +1,11 @@
-# must run as superuser
+# must run as superuser, but in home directory
 aptitude install build-essential python idle3 emacs xmonad dzen2 git
+cp .emacs ~/
+mkdir ~/.xmonad
+cp xmonad.hs ~/
+
+# install source code pro font
+chmod u+x installSourceCodePro.hs
+sh installSourceCodePro.hs
+
+# todo: copy xsession
