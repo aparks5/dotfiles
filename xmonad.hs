9d04eb2ad94f11e71d4a645b12bab0fa5a122cde
aparks5 <acousticscommander@gmail.com>
2015-11-25 22:04:36 -0500

    xmobar initialize hooks, docks

--- xmonad.hs
+++ xmonad.hs
@@ -1,12 +1,25 @@
 import XMonad
+import Data.Monoid
+import System.Exit
+
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
 
 main = do
+   xmproc <- spawnPipe "/usr/bin/xmobar /home/anthony/.xmobarrc"
    xmonad $ defaultConfig
      {
 	modMask = mod4Mask,
 	borderWidth = myBorderWidth,
-	focusedBorderColor = myFocusedBorderColor
+	focusedBorderColor = myFocusedBorderColor,
+        manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
      }
 
 myBorderWidth = 3
 myFocusedBorderColor = "#66ff66"
+
+
