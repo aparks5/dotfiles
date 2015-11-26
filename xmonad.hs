1b2d6afeda87359aa9ada9ffac01713ffab2be34
aparks5 <acousticscommander@gmail.com>
2015-11-25 22:51:12 -0500

    minimal

--- xmonad.hs
+++ xmonad.hs
@@ -9,17 +9,22 @@ import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
 main = do
-   xmproc <- spawnPipe "/usr/bin/xmobar /home/anthony/.xmobarrc"
+   xmproc <- spawnPipe myBar
    xmonad $ defaultConfig
      {
 	modMask = mod4Mask,
 	borderWidth = myBorderWidth,
 	focusedBorderColor = myFocusedBorderColor,
-        manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        manageHook = manageDocks <+> manageHook defaultConfig,
+        layoutHook = avoidStruts  $  layoutHook defaultConfig
      }
 
+
+myBar = "/usr/bin/xmobar /home/anthony/.xmobarrc"
 myBorderWidth = 3
 myFocusedBorderColor = "#66ff66"
+
+
+
 
 
