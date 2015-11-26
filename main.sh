a34d444eaca3615dfc53c4f8d62e1d9d2c270e40
aparks5 <acousticscommander@gmail.com>
2015-11-25 22:08:43 -0500

    update for xmobarrc

--- main.sh
+++ main.sh
@@ -4,8 +4,11 @@ aptitude install build-essential emacs python idle3 pylint \
     thunar gnome-icon-theme-full lxappearance
 
 sudo -u ${USERNAME} cp .emacs ~/
+
+# window manager config
 sudo -u ${USERNAME} mkdir ~/.xmonad
 sudo -u ${USERNAME} cp xmonad.hs ~/.xmonad/
+sudo -u ${USERNAME} cp .xmobarrc ~/
 
 # install source code pro font
 sudo -u ${USERNAME} chmod u+x installSourceCodePro.hs
