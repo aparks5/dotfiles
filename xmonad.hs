9a7a17bc41bb7f29028385f4c21a44e3ee98b912
aparks5 <acousticscommander@gmail.com>
2015-11-26 10:39:00 -0500

    resizable tiles vertically and horizontally with M- v b n m

--- xmonad.hs
+++ xmonad.hs
@@ -2,11 +2,16 @@ import XMonad
 import Data.Monoid
 import System.Exit
 
+-- spawn xmobar
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
+
+-- change vertical size
+import XMonad.Layout.ResizableTile
+import XMonad.Util.EZConfig -- or use another method of binding resizable keys
 
 main = do
    xmproc <- spawnPipe myBar
@@ -16,13 +21,22 @@ main = do
 	borderWidth = myBorderWidth,
 	focusedBorderColor = myFocusedBorderColor,
         manageHook = manageDocks <+> manageHook defaultConfig,
-        layoutHook = avoidStruts  $  layoutHook defaultConfig
-     }
+        layoutHook = myLayout
+      } `additionalKeysP` myKeys
 
+myLayout = ResizableTall 1 (2/100) (1/2) []
+-- avoidStruts  $ myLayout
 
+myKeys =  -- resize both axes in resizableTall
+    [ ("M-m", sendMessage $ MirrorExpand)
+    , ("M-n", sendMessage $ MirrorShrink)
+    , ("M-v", sendMessage $ Shrink)
+    , ("M-b", sendMessage $ Expand)
+    ]
+ 
 myBar = "/usr/bin/xmobar /home/anthony/.xmobarrc"
 myBorderWidth = 3
-myFocusedBorderColor = "#66ff66"
+myFocusedBorderColor = "#669999"
 
 
 
