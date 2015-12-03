ac900cc1c286cf4a64c6ef961505fc6d3d9eb61f
aparks5 <acousticscommander@gmail.com>
2015-12-03 09:12:47 -0500

    file name errors

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 # must run as superuser, but in home directory
-aptitude install build-essential emacs python idle3 pylint \ 
-    xmonad dzen2 xscreensaver suckless-tools \
-    thunar gnome-icon-theme-full lxappearance
+#aptitude install build-essential emacs python idle3 pylint \ 
+#    xmonad dzen2 xscreensaver suckless-tools \
+#    thunar gnome-icon-theme-full lxappearance
 
 sudo -u ${USERNAME} cp .emacs ~/
 
@@ -11,8 +11,8 @@ sudo -u ${USERNAME} cp xmonad.hs ~/.xmonad/
 sudo -u ${USERNAME} cp .xmobarrc ~/
 
 # install source code pro font
-sudo -u ${USERNAME} chmod u+x installSourceCodePro.hs
-sudo -u ${USERNAME} sh installSourceCodePro.hs
+sudo -u ${USERNAME} chmod u+x installAdobeSourceCodePro.sh
+sudo -u ${USERNAME} sh installAdobeSourceCodePro.sh
 
 # todo: copy xsession
 sudo -u ${USERNAME} cp .xsession ~/
