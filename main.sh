420d0e98078d0497b671868afe169a175a225baa
aparks5 <acousticscommander@gmail.com>
2015-12-03 10:39:08 -0500

    chown ownership issues

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 # must run as superuser, but in home directory
-#aptitude install build-essential emacs python idle3 pylint \ 
-#    xmonad dzen2 xscreensaver suckless-tools \
-#    thunar gnome-icon-theme-full lxappearance
+aptitude install build-essential emacs python idle3 pylint \ 
+    xmonad dzen2 xscreensaver suckless-tools \
+    thunar lxde gnome-icon-theme-full lxappearance
 
 sudo -u ${USERNAME} cp .emacs ~/
 
@@ -10,9 +10,12 @@ sudo -u ${USERNAME} mkdir ~/.xmonad
 sudo -u ${USERNAME} cp xmonad.hs ~/.xmonad/
 sudo -u ${USERNAME} cp .xmobarrc ~/
 
+sudo chown -R anthony ~/.xmonad/
+
 # install source code pro font
 sudo -u ${USERNAME} chmod u+x installAdobeSourceCodePro.sh
 sudo -u ${USERNAME} sh installAdobeSourceCodePro.sh
 
 # todo: copy xsession
 sudo -u ${USERNAME} cp .xsession ~/
+sudo chown -R anthony ~/.xmonad/
